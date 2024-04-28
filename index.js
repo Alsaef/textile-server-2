3d93833753fe34aaccabf1e82d2374fdab6a6392
ASR <saef.ratul@gmail.com>
2024-04-28 23:08:33 +0600

    dev

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-const port =process.env.PORT|| 3000
+const port =process.env.PORT|| 6000
 
 app.use(express.json())
 app.use(cors())
